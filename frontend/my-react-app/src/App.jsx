531b51db049a34929447d8370ecc5ffeeb22e458
LOGITH AADITHYA S <logithjee@gmail.com>
2025-09-10 16:57:34 +0530

    ui update

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import AuthForms from "./assets/components/Authforms";
+import "./assets/Styles/authform.css"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <AuthForms />
     </>
   )
 }
