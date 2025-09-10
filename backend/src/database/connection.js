6fb24467cc5ca4dfecd18f6081c582ac862ff3c1
Yogesh <yogeshkumar.n1234@gmail.com>
2025-09-10 16:40:08 +0530

    db_set_up

---
+++ backend/src/database/connection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionObj = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+    );
+    console.log(`Successfully connected to database`);
+  } catch (error) {
+    console.log("Database connection failed.");
+    process.exit(1);
+  }
+};
+
+export default connectDB;
