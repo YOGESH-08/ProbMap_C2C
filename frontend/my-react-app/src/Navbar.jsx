506f8ac164a1ac2ac13edd60d919c1fa5553b344
Arvind-2006 <arvindm04032006@gmail.com>
2025-09-10 23:21:45 +0530

    Reapply "User Update"
    This reverts commit 6e6ca2dcb9cf42ec89ca827311ee9008315b11b5.

---
+++ frontend/my-react-app/src/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './Navbar.css'
+
+function Navbar(){
+    return(
+        <>
+        <div className="container-nav">
+            <div className="left-nav">
+                ProbMap
+            </div>
+            <div className="mid-nav">
+                <div className="home">Home</div>
+                <div className="report">Report</div>
+                <div className="solution">History</div>
+            </div>
+
+            <div className="contact-button" id="Login">Login</div>
+        </div>
+            
+        </>
+    );
+}
+
+export default Navbar
