8f052f48a262ab65ccefed1f71a7cb92cacd47bc
Arvind-2006 <arvindm04032006@gmail.com>
2025-09-11 18:23:16 +0530

    pop up

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -24,12 +24,17 @@ function Home1() {
 
 function App() {
   const [user, setUser] = useState(null);
-
+  const [showPopup, setShowPopup] = useState(false);
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
     });
-    return () => unsubscribe(); 
+    // Show popup after 3 seconds
+    const timer = setTimeout(() => setShowPopup(true), 3000);
+    return () => {
+      unsubscribe();
+      clearTimeout(timer);
+    };
   }, []);
 
   // if (!user) {
@@ -37,17 +42,20 @@ function App() {
   // }
 
   return (
-    // <AuthForms/>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home1 />} />
-        <Route path="/report" element={<Upload />} />
-        <Route path="/pending" element={<UserIssues filterStatus="pending" />} />
-        <Route path="/history" element={<UserIssues filterStatus="history" />} />
-      </Routes>
-    </Router>
+    <>
+      {showPopup && <Popup onClose={() => setShowPopup(false)} />}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home1 />} />
+          <Route path="/report" element={<Upload />} />
+          <Route path="/pending" element={<UserIssues filterStatus="pending" />} />
+          <Route path="/history" element={<UserIssues filterStatus="history" />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
+import Popup from "./popup";
 export default App;
